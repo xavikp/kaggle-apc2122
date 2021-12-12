5f2b78e0378960560fe8bfa5e3b273bfc8bcca70
xavier.val <1xaviervalparejo@gmail.com>
2021-12-12 22:13:28 +0100

    bug fix

--- demo/demo.py
+++ demo/demo.py
@@ -39,17 +39,16 @@ Home Net R: xxx
 Away Net Y: xxx
 Away Net R: xxx
 '''
-if(args.svm != 0):
-    hTop = pd.Series(float(input("Home ToP:")))
-    aTop = pd.Series(float(input("Away ToP:")))
-    aSco = pd.Series(int(input("Away Score:")))
-    hFR = pd.Series(int(input("Home 1st Rushing:")))
-    aFR = pd.Series(int(input("Away 1st Rushing:")))
-    hNR = pd.Series(int(input("Home Net Rushing Yards:")))
-    aNY = pd.Series(int(input("Away Net Yards:")))
-    aNR = pd.Series(int(input("Away Net Rushing Yards:")))
+hTop = pd.Series(float(input("Home ToP:")))
+aTop = pd.Series(float(input("Away ToP:")))
+aSco = pd.Series(int(input("Away Score:")))
+hFR = pd.Series(int(input("Home 1st Rushing:")))
+aFR = pd.Series(int(input("Away 1st Rushing:")))
+hNR = pd.Series(int(input("Home Net Rushing Yards:")))
+aNY = pd.Series(int(input("Away Net Yards:")))
+aNR = pd.Series(int(input("Away Net Rushing Yards:")))
     
-    header = [
+header = [
             "Home ToP", "Away ToP", 
             "away Score",
             "Home 1st R",
@@ -57,13 +56,16 @@ if(args.svm != 0):
             "Home Net R",
             "Away Net Y","Away Net R", 
           ]
-    game = pd.concat([
+game = pd.concat([
             hTop, aTop, aSco, hFR, aFR, hNR, aNY, aNR
             ],
             axis=1, keys=header)
 
+if(args.svm != 0 and args.rf != 0 and args.knn != 0):
     poly, rbf, x_test, y_test, y_train = tm.train_SVM(df, False)
     y_pred = poly.predict(game)
+    if(rbf.predict(game) != poly.predict(game)):
+        print("Seems that there's some discrepancies...")
     if(y_pred != 1):
         print("SVM with poly kernel predicts the result to be victory to the AWAY team!")
     else:
@@ -74,35 +76,6 @@ if(args.svm != 0):
         print("SVM with rbf kernel predicts the result to be victory to the AWAY team!")
     else:
         print("SVM with rbf kernel predicts the result to be victory to the HOME team!")
-    
-    if(rbf.predict(game) != poly.predit(game)):
-        print("Seems that there's some discrepancies...")
-
-
-    
-
-if(args.rf != 0):
-    hTop = pd.Series(float(input("Home ToP:")))
-    aTop = pd.Series(float(input("Away ToP:")))
-    aSco = pd.Series(int(input("Away Score:")))
-    hFR = pd.Series(int(input("Home 1st Rushing:")))
-    aFR = pd.Series(int(input("Away 1st Rushing:")))
-    hNR = pd.Series(int(input("Home Net Rushing Yards:")))
-    aNY = pd.Series(int(input("Away Net Yards:")))
-    aNR = pd.Series(int(input("Away Net Rushing Yards:")))
-    
-    header = [
-            "Home ToP", "Away ToP", 
-            "away Score",
-            "Home 1st R",
-            "Away 1st R",
-            "Home Net R",
-            "Away Net Y","Away Net R", 
-          ]
-    game = pd.concat([
-            hTop, aTop, aSco, hFR, aFR, hNR, aNY, aNR
-            ],
-            axis=1, keys=header)
 
     random_forest, x_test, y_test, y_train = tm.train_RF(df, 300)
     y_pred = random_forest.predict(game)
@@ -110,34 +83,48 @@ if(args.rf != 0):
         print("Random Forests predicts the result to be victory to the AWAY team!")
     else:
         print("Random Forests predicts the result to be victory to the HOME team!")
-
-
-if(args.knn !=0):
-    hTop = pd.Series(float(input("Home ToP:")))
-    aTop = pd.Series(float(input("Away ToP:")))
-    aSco = pd.Series(int(input("Away Score:")))
-    hFR = pd.Series(int(input("Home 1st Rushing:")))
-    aFR = pd.Series(int(input("Away 1st Rushing:")))
-    hNR = pd.Series(int(input("Home Net Rushing Yards:")))
-    aNY = pd.Series(int(input("Away Net Yards:")))
-    aNR = pd.Series(int(input("Away Net Rushing Yards:")))
     
-    header = [
-            "Home ToP", "Away ToP", 
-            "away Score",
-            "Home 1st R",
-            "Away 1st R",
-            "Home Net R",
-            "Away Net Y","Away Net R", 
-          ]
-    game = pd.concat([
-            hTop, aTop, aSco, hFR, aFR, hNR, aNY, aNR
-            ],
-            axis=1, keys=header)
-
     knn, x_test, y_test, y_train = tm.train_KNN(df, 15)
     y_pred = knn.predict(game)
     if(y_pred != 1):
         print("KNN predicts the result to be victory to the AWAY team!")
     else:
         print("KNN predicts the result to be victory to the HOME team!")
+    
+else:
+    if(args.svm != 0):        
+        poly, rbf, x_test, y_test, y_train = tm.train_SVM(df, False)
+        y_pred = poly.predict(game)
+        if(rbf.predict(game) != poly.predict(game)):
+            print("Seems that there's some discrepancies...")
+        if(y_pred != 1):
+            print("SVM with poly kernel predicts the result to be victory to the AWAY team!")
+        else:
+            print("SVM with poly kernel predicts the result to be victory to the HOME team!")
+
+        y_pred = rbf.predict(game)
+        if(y_pred != 1):
+            print("SVM with rbf kernel predicts the result to be victory to the AWAY team!")
+        else:
+            print("SVM with rbf kernel predicts the result to be victory to the HOME team!")
+    
+    if(args.rf != 0):
+        random_forest, x_test, y_test, y_train = tm.train_RF(df, 300)
+        y_pred = random_forest.predict(game)
+        if(y_pred != 1):
+            print("Random Forests predicts the result to be victory to the AWAY team!")
+        else:
+            print("Random Forests predicts the result to be victory to the HOME team!")
+
+
+    if(args.knn !=0):
+        knn, x_test, y_test, y_train = tm.train_KNN(df, 15)
+        y_pred = knn.predict(game)
+        if(y_pred != 1):
+            print("KNN predicts the result to be victory to the AWAY team!")
+        else:
+            print("KNN predicts the result to be victory to the HOME team!")
+
+
+    
+
